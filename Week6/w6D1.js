7282c77b95bf307c8f9a4cd24d41ef8263c0f735
Andrew T <me@andrewtran-shi.dev>
2023-05-16 08:49:51 -0700

    week 6

--- Week6/w6D1.js
+++ Week6/w6D1.js
@@ -24,16 +24,17 @@ const expected2 = -1;
  * @returns {number} The balance index or -1 if there is none.
  */
 function balanceIndex(nums) {
-  debugger
   for (let i = 0; i < nums.length; i++) {
     // loop through array
     // counter
     let leftSum = 0; // left side of index
     let rightSum = 0; // right side of index
-    for (let j = 0; j < i; j++) { //we start this loop at 0 because we want to include the index itself (j i K) during the first iteration the left side will be 0
+    for (let j = 0; j < i; j++) {
+      //we start this loop at 0 because we want to include the index itself (j i K) during the first iteration the left side will be 0
       leftSum += nums[j]; // left side of index
     }
-    for (let k = i + 1; k < nums.length; k++) { // we start at i + 1 because we don't want to include the index itself
+    for (let k = i + 1; k < nums.length; k++) {
+      // we start at i + 1 because we don't want to include the index itself
       // right side of index
       rightSum += nums[k]; // right side of index
     }
@@ -42,7 +43,6 @@ function balanceIndex(nums) {
     }
   }
   return -1;
-  debugger
 }
 
 console.log(balanceIndex(nums1));
