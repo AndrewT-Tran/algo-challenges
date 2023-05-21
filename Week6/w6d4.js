6a247f5c093e4aac7081fc6a222eae6d1e71e3f5
Andrew T Tran <me@andrewtran-shi.dev>
2023-05-21 03:35:43 -0700

    new file:   Week6/w6d4.js

---
+++ Week6/w6d4.js
@@ -0,0 +1,70 @@
+/* 
+Array: Binary Search (non recursive)
+
+    Given a sorted array and a value, return whether the array contains that value.
+    Do not sequentially iterate the array. Instead, ‘divide and conquer’,
+    taking advantage of the fact that the array is sorted .
+
+    Bonus (alumni interview): 
+    first complete it without the bonus, because they ask for additions
+    after the initial algo is complete
+
+    return how many times the given number occurs
+*/
+
+const nums1 = [1, 3, 5, 6];
+const searchNum1 = 4;
+const expected1 = false;
+
+const nums2 = [4, 5, 6, 8, 12];
+const searchNum2 = 5;
+const expected2 = true;
+
+const nums3 = [3, 4, 6, 8, 12];
+const searchNum3 = 3;
+const expected3 = true;
+
+// bonus, how many times does the search num appear?
+const nums4 = [2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9];
+const searchNum4 = 2;
+const expected4 = 4;
+
+/**
+ * Efficiently determines if the given num exists in the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} sortedNums
+ * @param {number} searchNum
+ * @returns {boolean} Whether the given num exists in the given array.
+ */
+function binarySearch(sortedNums, searchNum) {
+    var left = 0;
+    var right = sortedNums.length - 1;
+    var search = 0;
+    for (var i = 0; i < sortedNums.length; i++) {
+        var mid = Math.floor((left + right) / 2);
+        if (sortedNums[mid] == searchNum) {
+            search++;
+            console.log(search);
+            return true;
+        } else if (sortedNums[mid] < searchNum) {
+            left = mid + 1;
+        } else if (sortedNums[mid] > searchNum) {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+console.log(binarySearch(nums1, searchNum1))    
+console.log(binarySearch(nums2, searchNum2))
+console.log(binarySearch(nums3, searchNum3))
+console.log(binarySearch(nums4, searchNum4))
+/*****************************************************************************/
+
+    // A while loop is appropriate here because we don't know the necessary length of the array!
+    // Assume the array is SORTED lowest to highest!
+
+    // With divide and conquer, a good place to start is the MIDDLE!
+
+
